5a3335663d9874a156dba0895a36f6b5683c91d0
donggyulee <nninnnin7@gmail.com>
2025-01-31 15:01:26 +0900

    컴포넌트 의존성 수정: 레이아웃 컴포넌트에 네비게이션 컴포넌트 포함

--- src/components/GlobalLayout.js
+++ src/components/GlobalLayout.js
@@ -1,20 +1,34 @@
-import Base from "@/components/Base.js";
+import { LitElement, html, css } from "lit";
+import { customElement } from "lit/decorators.js";
 
-class GlobalLayout extends Base {
-  constructor() {
-    super();
-  }
+import "@/components/Navigation.js";
+
+@customElement("global-layout")
+class GlobalLayout extends LitElement {
+  static styles = css`
+    :host {
+      margin: 0;
+      padding: 0;
+
+      display: flex;
+      flex-direction: column;
+      min-height: 100dvh;
+
+      position: relative;
+    }
+
+    :host div {
+      width: 600px;
+      margin: 0 auto;
+    }
+  `;
 
   render() {
-    this.shadowRoot.innerHTML += `
-      <style>
-        :host {
-          margin: 0;
-          padding: 0;
-        }
-      </style>
-
-      <slot></slot>
+    return html`
+      <my-navigation></my-navigation>
+      <div>
+        <slot></slot>
+      </div>
     `;
   }
 }
